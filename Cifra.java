871aea44839b38065e83b68727ee55700b2db8ec
gabrielseffrin <gabrielfseffrin@gmail.com>
2022-11-28 20:15:38 -0300

    leitura de arquivo

--- Cifra.java
+++ Cifra.java
@@ -1,8 +1,27 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.nio.CharBuffer;
+
 /**
  * Cifra
  */
 public class Cifra {
 
+    // cifra para decriptar e encriptar
+    private int cifra;
+
+    private File arquivo;
+    private final String NOME_ARQUIVO = "text.txt";
+
+    // construtor para criar o arquivo.txt e atribuir a cifra
+    public Cifra(int cifra) {
+        this.cifra = cifra;
+        arquivo = new File(arquivo, NOME_ARQUIVO);
+    }
+
+    // unica entrada: do usuário fora o arquivo.txt
     public void executar(int opcao) {
 
         switch (opcao) {
@@ -17,12 +36,29 @@ public class Cifra {
         }
     }
 
-    private String replace(String string, String substituir, String substituido) {
-        return string.replaceAll(substituir, substituido);
+    // método para substituir os caracteres
+    private ListCaracters substituicaoCaracteres(String string, String substituir, String substituido) {
+
+        ListCaracters listCaracters = new ListCaracters();
+
+        try {
+            // cria um leitor do arquivo
+            FileReader reader = new FileReader(arquivo);
+            // buffer para agilizar a leitura
+            BufferedReader bReader = new BufferedReader(reader);
+            System.out.println(bReader.readLine());
+
+            // fecha o buffer rader
+            bReader.close();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        return null;
     }
 
     private void encriptar() {
-        replace(null, null, null);
+        substituicaoCaracteres(null, null, null);
     }
 
     private void decriptar() {
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cifra cifra = new Cifra(2);
+
+        cifra.executar(0);
+    }
+}
